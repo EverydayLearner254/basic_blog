aa6062092d68708cd2b6a0f7626d4814f8872bb4
AlexKamuhu <akamuhu442@gmail.com>
2020-05-03 23:55:21 +0300

    tinymce base dir fix

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -133,7 +133,7 @@ VENV_PATH = os.path.join(BASE_DIR)
 STATIC_ROOT = os.path.join(VENV_PATH, 'static/_root/')
 MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
 
-STATIC_JS_DIR = os.path.join(STATICFILES_DIRS, "js")
+STATIC_JS_DIR = os.path.join(os.path.join(BASE_DIR, 'static'), "js")
 TINYMCE_JS_ROOT = os.path.join(STATIC_JS_DIR, "tiny_mce")
 TINYMCE_JS_URL = os.path.join(TINYMCE_JS_ROOT, "tiny_mce.js")
 
